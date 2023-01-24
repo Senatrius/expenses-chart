5633c419d53aa06ccde94bd9be840f50c06a1842
senatrius <g.makutenas@gmail.com>
2023-01-24 15:27:53 +0200

    add file upload functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import './index.css';
 
 function App() {
+  const [customMode, setCustomMode] = useState<boolean>(false);
+
   return (
     <div className='mx-auto flex min-h-full w-[91.5%] flex-col items-center justify-center py-[4.1875rem] md:w-[60%] lg:w-[45%] xl:w-[37.5%]'>
+      <button
+        onClick={() => setCustomMode(!customMode)}
+        className='mb-1 self-end bg-primary-red p-1'>
+        Switch
+      </button>
       <Header />
-      <Main />
+      <Main customMode={customMode} />
     </div>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,16 +2,49 @@ import { Chart } from './Chart';
 import chartData from '../data.json';
 import { Divider } from './Divider';
 import { Summary } from './Summary';
+import { useDropzone } from 'react-dropzone';
 
-export const Main = () => {
+export const Main = ({ customMode }: { customMode: boolean }) => {
+  const onDrop = (acceptedFiles: any) => {
+    console.log(acceptedFiles);
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      'text/csv': ['.csv']
+    }
+  });
+
   return (
     <main className='w-full rounded-lg bg-component p-5 md:mb-6 md:py-7 md:px-8'>
-      <h2 className='mb-4 text-card-title-m text-text md:mb-2 md:text-card-title-d'>
-        Spending - Last 7 days
-      </h2>
-      <Chart data={chartData} />
-      <Divider />
-      <Summary />
+      {!customMode && (
+        <>
+          <h2 className='mb-4 text-card-title-m text-text md:mb-2 md:text-card-title-d'>
+            Spending - Last 7 days
+          </h2>
+          <Chart data={chartData} />
+          <Divider />
+          <Summary />
+        </>
+      )}
+      {customMode && (
+        <div
+          className='m-0 flex h-[9.375rem] w-full items-center justify-center rounded-md border-2 border-dashed border-label p-3 text-center md:h-[15rem]'
+          {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p className='text-card-chart-m text-label md:text-card-chart-d'>
+              Drop the files here ...
+            </p>
+          ) : (
+            <p className='text-card-chart-m text-label md:text-card-chart-d'>
+              Drag 'n' drop some files here, or click to select files. Accepted
+              file format: [*.csv]
+            </p>
+          )}
+        </div>
+      )}
     </main>
   );
 };
